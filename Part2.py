b89a8f320c5ba6c42bb403efec81bca3311ac2f6
unknown <bitanyabdemissie@gmail.com>
2026-04-09 22:37:10 +0400

    Save local project files before merging remote

---
+++ Part2.py
@@ -0,0 +1,38 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow,QLabel
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)
+
+        label = QLabel("Hello", self)
+        label.setFont(QFont("Times New Roman", 40))
+        label.setGeometry(0, 0, 500, 100)
+        label.setStyleSheet("color: #b25add;"
+                            "background-color: #c8c897;"
+                            "font-weight: bold;"
+                            "font-style: italic;"
+                            "text-decoration: underline;")
+        #label.setAlignment(Qt.AlignTop)
+        #label.setAlignment(Qt.AlignBottom)
+        #label.setAlignment(Qt.AlignVCenter)
+
+        #label.setAlignment(Qt.AlignRight)
+        #label.setAlignment(Qt.AlignLeft)
+        #label.setAlignment(Qt.AlignHCenter)
+
+        label.setAlignment(Qt.AlignHCenter|Qt.AlignVCenter)#label.setAlignment(Qt.AlignCenter)
+
+        
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
